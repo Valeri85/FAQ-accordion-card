35bb4ca10b0547245e51de118cd21dee802a3972
Valerimakaridze <valeriousmonk85@gmail.com>
2022-03-08 18:25:07 +0400

    Improve accordion functionality, Refactoring

--- js/script.js
+++ js/script.js
@@ -1,35 +1,45 @@
-// 'use strict';
+'use strict';
 
 // Select accordion
 const FAQ = document.querySelector('.main__faq');
 // select questions
 const buttons = FAQ.querySelectorAll('.main__faq-button');
+// select answers
+const answers = FAQ.querySelectorAll('.main__faq-answer');
 
-// Loop through all buttons
-buttons.forEach(button =>
-	button.addEventListener('click', event => {
-		// Select container of current question and answer
-		const parentContainer = event.target.closest('.main__faq-item');
-		// Select current answer
-		const currentAnswer = parentContainer.querySelector('.main__faq-answer');
+// Loop through buttons
+buttons.forEach(currentButton =>
+	// Add event listeners on current buttons
+	currentButton.addEventListener('click', event => {
+		// Select current FAQ items
+		const currentFAQItem = event.target.closest('.main__faq-item');
+		// Select current answers
+		const currentAnswer = currentFAQItem.querySelector('.main__faq-answer');
 
-		// Toggle active class on buttons
-		button.classList.toggle('active');
+		// Loop through buttons
+		buttons.forEach(button => {
+			// If button is not current button
+			button !== currentButton
+				? button.classList.remove('active') // Remove 'active' class from all buttons
+				: currentButton.classList.toggle('active'); // Toggle 'active' class on current button
 
-		// Toggle hide class on answars
-		currentAnswer.classList.toggle('show');
+			// If button is active
+			button.classList.contains('active')
+				? button.setAttribute('aria-expanded', true) // Aria-expanded attribute set to true
+				: button.setAttribute('aria-expanded', false); // Aria-expanded attribute set to false if button is not active
+		});
 
-		// Toggle aria attributes
-		if (button.classList.contains('active')) {
-			// Set aria-expanded attribute to true on buttons
-			button.setAttribute('aria-expanded', true);
-			// Set aria-hidden attribute to false on answers
-			currentAnswer.setAttribute('aria-hidden', false);
-		} else {
-			// Set aria-expanded attribute to true on buttons
-			button.setAttribute('aria-expanded', false);
-			// Set aria-hidden attribute to false on answers
-			currentAnswer.setAttribute('aria-hidden', true);
-		}
+		// Loop through answers
+		answers.forEach(answer => {
+			// If answer is not current answer
+			answer !== currentAnswer
+				? answer.classList.remove('show') // Remove 'show' class from all answers
+				: currentAnswer.classList.toggle('show'); // Toggle 'show' class on current answer
+
+			// If answer is shown
+			answer.classList.contains('show')
+				? answer.setAttribute('aria-hidden', false) // Aria-hidden attribute set to false
+				: answer.setAttribute('aria-hidden', true); // Aria-hidden attribute set to true if answer is not shown
+		});
 	})
 );
